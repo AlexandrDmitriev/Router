64c8554dd567588d9f781b4f934ca836e7f436d6
avreliy <avreliy.89@gmail.com>
2014-03-17 21:04:26 +0200

    Add gitignore

---
+++ Router.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Router;
+
+use CoreInterfaces\IRouter;
+
+class Router implements IRouter
+{
+
+    public function run(callable $patternConstructorDelegate)
+    {
+        // TODO: Implement run() method.
+    }
+
+    public function getUrl($controller, $action)
+    {
+        // TODO: Implement getUrl() method.
+    }
+}
